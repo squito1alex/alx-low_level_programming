0ba945da0281caa0fae57b22e541e2ba32c31ea1
squito1alex <ejorucalex@gamil.com>
2022-10-13 11:56:34 -0700

    Betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Serves as the empty point for the program
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
+ */
+int main (void)
+{
+	puts("\" programming is like building amultilingual puzzle");
+	return (0)
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ *main-is the entry point for the program.
+ *
+ * Return:zero if no error, Non-zero value if errors.
+ */
+int main (void)
+{
+	printf("Size of a char: %ld byte(s)\n", Sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\", sizeof(float);
+			return (0);
+			}
