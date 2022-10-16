0645ecf09f57c5835032278d21c6b132e8dfed82
squito1alex <ejorucalex@gamil.com>
2022-10-16 04:28:33 -0700

    First

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: By default returns zero if no errors otherwise returns non-zero value.
  */
-int main (void)
+int main(void)
 {
-	puts("\" programming is like building amultilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
