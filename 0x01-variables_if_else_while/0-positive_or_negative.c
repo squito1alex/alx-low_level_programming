fbcd717afa2123fe039ae969937cb4f1f41e47e7
squito1alex <ejorucalex@gamil.com>
2022-10-16 11:48:46 -0700

    God

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - entry point
- * description: print postive or negative number
+ * description: print positive or negative number
  * Return:0
  */
 int main(void)
@@ -14,10 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is postive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
