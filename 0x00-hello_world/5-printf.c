a4e571c0f5195b886130e78845ce64a3dbd9996a
squito1alex <ejorucalex@gamil.com>
2022-10-16 10:00:15 -0700

    Third

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main-Serves as the empty point for the program
  *
- * Return:By default returns zero of no errors otherwise returns non-zero value.
+ * Return:By default returns zero of no errors.
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
