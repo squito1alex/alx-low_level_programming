fbf4b0bee44c72b56b132d32b6c4f55d39ae5c82
squito1alex <ejorucalex@gamil.com>
2022-10-16 09:36:42 -0700

    Second

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main-Serves as the empty point for the program
  *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
+ * Return: By default returns zero if no errors.
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzles");
+	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
