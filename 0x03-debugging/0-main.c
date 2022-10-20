cde27eb89de03d22ac1d788d61e9c65979b6fc59
squito1alex <ejorucalex@gamil.com>
2022-10-20 12:02:10 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	if (i = 98)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+DEBUGGING
+      Is aprocess of finding and resolving bugs (defects or problems that prevent correct operation) within computer programs, software or systems.
