ceeb08e05754e20410bc094be389121b91924c65
squito1alex <ejorucalex@gamil.com>
2022-10-21 02:55:28 -0700

    good

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,4 +7,10 @@ void postive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+int main(void);
+
+int convert_day(int month, int day);
+
+void print_remmaining_days(int month, int day, int year);
+
 #endif /*MAIN*/
