3d46e16a41487d78ae1a4053ff6cb71863e29205
squito1alex <ejorucalex@gamil.com>
2022-10-21 13:20:36 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
  * main - print_alphabet in lowercase
+ * new line
  */
+
 void print_alphabet(void)
 {
+
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
