868a3f133eb82ab6e5171b159aaa45c12ca7c480
squito1alex <ejorucalex@gmail.com>
2022-11-07 14:57:39 -0800

    good

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest: the place we xopy to.
+ * @src: we copy from
+ * @n: how much we copy
+ *
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsingned int n)
+{
+	char *ptr = dest;
+
+	while (n -- > 0)
+	{
+		*dest = *src;
+
+		dest++;
+		src++;
+	}
+	return (ptr);
+}
